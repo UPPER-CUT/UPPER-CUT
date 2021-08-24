0fe93e668354d57b0616ad230b66dac1f33f79c4
UPPER-CUT <sergiotorugnoing@gmail.com>
2021-08-24 11:45:40 -0600

    Envio por  v0.5

--- datos.php
+++ datos.php
@@ -3,18 +3,31 @@
 
    
       $nombre_img=$_FILES['img']['name']; //guarda el nombre de la imagen.//
-      $tipo_imagen=$_FILES['img']['type'];//guarda el tipo de la imagen//
-      $tamagno_imagen=$_FILES['img']['size'];//guarda el tamaño de la imagen//
+      $tipo_imagen=$_FILES['img']['type']."<br>";//guarda el tipo de la imagen//
+      $tamagno_imagen=$_FILES['img']['size']."<br>";//guarda el tamaño de la imagen//
 
-      //echo  $_SERVER['DOCUMENT_ROOT'].PHP_EOL;//
+    if($tamagno_imagen=$_FILES['img']['size']<=3000000){
 
-      $carpeta_destino=$_SERVER['DOCUMENT_ROOT'].'/practicas/Imagenes/Img/'; //Ruta de destino donde se almacenara la imgen//
+      if($tamagno_imagen="image/jpeg" || $tamagno_imagen="image/png" ||$tipo_imagen="image/gif"){
+            //echo  $_SERVER['DOCUMENT_ROOT'].PHP_EOL;//
+            
+            $carpeta_destino=$_SERVER['DOCUMENT_ROOT'].'/practicas/Imagenes/Img/'; 
+            //Ruta de destino donde se almacenara la imgen//
 
-      move_uploaded_file($_FILES['img']['tmp_name'], $carpeta_destino.$nombre_img);
-      /*La funcion move uploaded_file realiza la funicion  de  trasaladar el archivo de la carpeta
-      temporal hacia la carpeta de destino. */
+            move_uploaded_file($_FILES['img']['tmp_name'], $carpeta_destino.$nombre_img);
+            /*La funcion move uploaded_file realiza la funicion  de  trasaladar el archivo de la carpeta
+            temporal hacia la carpeta de destino. */
+      }
+      else{
 
-      echo  $nombre_img; //imprimo el nombre de la imagen//
+            echo"Error:El formato que haz ingresado se soporta por el servidor";
+      }
+    }
+
+    else{
+
+      echo "Error:No se pude Enviar archivos de mas de 3MB";
+    }
 
 
 ?>
